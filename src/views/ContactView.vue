d9d8605a158c3e7c812be3d0b04418889f45f649
JusticePlague <43143136+JusticePlague@users.noreply.github.com>
2023-08-22 11:05:10 -0400

    DB change

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,16 +1,11 @@
 <script setup>
-
 import TheContact from '../components/contactcomp/TheContact.vue'
-
-
 </script>
 
 <template>
   <main>
     <TheContact />
   </main>
-
-  
 </template>
 
 <style scoped></style>
