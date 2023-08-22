7d40fee383c51a3a660d548fe38e4784b0d4dc6c
JusticePlague <43143136+JusticePlague@users.noreply.github.com>
2023-08-22 11:08:46 -0400

    DB

--- src/views/CharacterListView.vue
+++ src/views/CharacterListView.vue
@@ -9,26 +9,28 @@ defineProps({
 
 <template>
     <div class="character">
-      <h2>{{ character.name }}</h2>
-      <span>{{ character.fullname }} on {{ event.date }}</span>
+        {{ character.name }}
+        {{ character.fullname }} 
+        {{ character.alignment }}
+        {{ character.description }}
+        {{ character. }}
+        {{ character. }}
+        {{ character. }}
+        {{ character. }}
+        {{ character. }}
+        {{ character. }}
+
+
+        "id": 
+        "name":
+        "full-name": 
+        "alignment": 
+        "description":
+        "shortbio": 
+        "location": 
     </div>
 </template>
 
 <style scoped>
-.event-card {
-  padding: 20px;
-  width: 250px;
-  cursor: pointer;
-  border: 1px solid #39495c;
-  margin-bottom: 18px;
-}
-.event-card:hover {
-  transform: scale(1.01);
-  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
-}
 
-.event-link {
-  color: #2c3e50;
-  text-decoration: none;
-}
 </style>
